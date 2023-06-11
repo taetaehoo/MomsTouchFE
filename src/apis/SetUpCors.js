f5e5f49a11e4ee57df0c21f3179a6363b1c72b88
taetaehoo <hapkldo09@naver.com>
2023-06-11 17:33:02 +0900

    d

---
+++ src/apis/SetUpCors.js
@@ -0,0 +1,9 @@
+import {createProxyMiddleware} from "http-proxy-middleware";
+
+module.exports = app => {
+    app.use('/api',
+        createProxyMiddleware({
+            target: 'http://localhost:8071',
+            changeOrigin: true,
+        }))
+}
